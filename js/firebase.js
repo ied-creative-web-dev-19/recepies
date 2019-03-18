9e208c3f7e0093b66a9fe5e83ea19800d93c6904
Giacomo Matthew Degl'Innocenti <info@giacomodeglinnocenti.com>
2019-03-18 10:16:39 +0100

    Create Project Backbone Folders

---
+++ js/firebase.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyAXZFH_U-bzQZZl-E_KQ3-KHM9P_WSEul4",
+  authDomain: "recepies-583eb.firebaseapp.com",
+  databaseURL: "https://recepies-583eb.firebaseio.com",
+  projectId: "recepies-583eb",
+  storageBucket: "recepies-583eb.appspot.com",
+  messagingSenderId: "525811569138"
+};
+firebase.initializeApp(config);
