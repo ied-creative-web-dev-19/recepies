dde9ff9a6980eb77d8614f3a7e3b4f891dd6dc1b
Pamela <pmattii01@gmail.com>
2019-05-17 16:10:07 +0200

    improved recepie page to show the recepie

--- js/controllers/recepie.js
+++ js/controllers/recepie.js
@@ -1,41 +1,81 @@
-// This is the controller for the Recepie (Expanded content) view
+//this is the controller for the recepie (extended content) view
 
-/*
-
-  - Leggere il parametro ID
-  - Richiedere al DB la ricetta relativa all'ID
-  - Mostrare la ricetta nella view
+/*this page 
+    - leggere il parametro ID
+    - Richiede al db la ricetta relativa all'id
+    - mostrare la ricetta
 
 */
 
 var db = firebase.firestore();
 
-function loadRecepie(){
-  var recepieId = getIdFromURL();
-  db.collection('recepies')
-    .doc(recepieId)
-    .onSnapshot((doc) => {
-      var recepie = doc.data();
-      console.log(recepie);
-  });
+
+function loadRecipie(){
+    var recepieId = getIdFromUrl();
+    db.collection('recepies').doc(recepieId).onSnapshot(function(item)
+    {
+        var recepie = item.data();
+        var recepieId = findGetParameter('id');
+        console.log(recepieId);
+
+        addRecipie(recepie, recepieId)
+
+    });
 }
 
-function getIdFromURL(){
-  return findGetParameter('id');
+
+
+function getIdFromUrl(){
+
+    return findGetParameter('id');
+
 }
+
 
 function findGetParameter(parameterName) {
-  var result = null,
-      tmp = [];
-  location.search
-      .substr(1)
-      .split("&")
-      .forEach(function (item) {
-        tmp = item.split("=");
-        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
-      });
-  return result;
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+          tmp = item.split("=");
+          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const myParam = urlParams.get('myParam');
+
+    return result;
 }
 
-// init 
-loadRecepie();
+function addRecipie(recepie, recepieId){
+
+    var title = recepie.name;
+    var difficulty = recepie.difficulty;
+    var cost = recepie.cost;
+    var description = recepie.description;
+    //var images = recepie.images;
+    var people = recepie.people;
+    var preparationTime = recepie.preparationTime;
+    var cookingTime = recepie.cookingTime;
+    var rating = recepie.rating;
+    var numVotes = recepie.numVotes;
+    var ingredients = recepie.ingredients;
+
+    console.log(title);
+    $('#recepieName').text(title);
+    $('#recepieCost').text(cost + " €");
+    $('#recepieDesc').text(description);
+    $('#recepieDifficulty').text(difficulty);
+    $('#recepieRating').text(rating);
+    $('#recepiePrepTime').text("Preparation time is: " + preparationTime + " minutes");
+    $('#recepieCookTime').text("Cooking time is: " + cookingTime + " minutes");
+    $('#recepieVotes').text(numVotes);
+    $('#recepieIngredients').text(ingredients);
+    $('#recepiePeople').text(people)
+}
+
+
+
+loadRecipie();
