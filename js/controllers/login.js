ddecf1183b39a24e4123f6dcedcd26622a972bd4
Giacomo Matthew Degl'Innocenti <info@giacomodeglinnocenti.com>
2019-06-05 11:39:36 +0200

    Added behaviour to log in user

--- js/controllers/login.js
+++ js/controllers/login.js
@@ -10,10 +10,33 @@ function toggleSignUp(e){
   $('#logreg-forms .form-signup').toggle(); // display:block or none
 }
 
-$(()=>{
+function doLogin(e){
+  e.preventDefault();
+  //leggere i dati inseriti
+  var email = $('#inputEmail').val();
+  var pass = $('#inputPassword').val();
+  //validarli
+  if( email.length < 5 ){
+    alert('Per favore scrivi il tuo indirizzo email di almeno 5 caratteri.');
+    return false;
+  }
+  if( pass.length < 6 ){
+    alert('Per favore scrivi la tua password di almeno 6 caratteri.');
+    return false;
+  }
+  //tentare il login
+  login(email, pass);
+}
+
+$(() => {
   // Login Register Form
   $('#logreg-forms #forgot_pswd').click(toggleResetPswd);
   $('#logreg-forms #cancel_reset').click(toggleResetPswd);
   $('#logreg-forms #btn-signup').click(toggleSignUp);
   $('#logreg-forms #cancel_signup').click(toggleSignUp);
-})
+
+  //////
+  $('#button-signin').click(function(e){
+    doLogin(e);
+  });
+});
--- js/services/authentication.js
+++ js/services/authentication.js
@@ -10,3 +10,14 @@ function signup(email, password, name){
     alert('Errore durante la registrazione.');
   });  
 }
+
+function login(email, password){
+  auth.signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+    console.log(error);
+    alert('Dati di login non corretti o errore di sistema, si prega di ritentare con i dati corretti.');
+  });  
+}
