edeec2637a4d788d4a7f3344e9fd3605d89210df
Giacomo Matthew Degl'Innocenti <info@giacomodeglinnocenti.com>
2019-06-05 11:53:41 +0200

    Added behaviour in service to trigger on user state change

--- js/services/authentication.js
+++ js/services/authentication.js
@@ -21,3 +21,32 @@ function login(email, password){
     alert('Dati di login non corretti o errore di sistema, si prega di ritentare con i dati corretti.');
   });  
 }
+
+var appUser = null;
+
+function listenAuth(){
+  auth.onAuthStateChanged(function(user) {
+    if (user) {
+      appUser = user;
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      console.log("LOG IN",user);
+      // ...
+    } else {
+      appUser = null;
+      // User is signed out.
+      // ...
+      console.log("LOG OUT",user);
+    }
+  });
+}
+
+$(document).ready(function(){
+  listenAuth();
+});
